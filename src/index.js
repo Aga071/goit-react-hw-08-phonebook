13a1db9f56b47c25961cfb13204d83020b3ab0d8
Agata Warzyszyńska <agata.wlazlak@gmail.com>
2023-12-07 23:44:23 +0100

    goit-react-hw-08-v4

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ChakraProvider } from '@chakra-ui/react';
 
+const basename = '/goit-react-hw-08-phonebook';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ChakraProvider>
         <PersistGate persistor={storePersist} loading={null}>
-          <BrowserRouter>
+          <BrowserRouter basename={basename}>
             <App />
           </BrowserRouter>
         </PersistGate>
